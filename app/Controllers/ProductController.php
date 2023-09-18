a6c51da04fed2f1f3b48d78a38a3c19651efb0ce
laramel20 <hidalgolaramelissa2003@gmail.com>
2023-09-18 13:31:52 +1200

    view

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,14 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+    public function laramel20()
+    {
+        echo 'working';
+    }
     public function index()
     {
         //
---
+++ app/Views/errors/products.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>Product List</h1>
+</body>
+</html>
